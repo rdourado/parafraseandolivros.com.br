33e5bb87e62848e04582f3c085fdb8a963db12b8
Rafael Dourado <rdourado@gmail.com>
2014-02-03 20:04:35 -0200

    Forgot footer links.

--- dynamic/wp-content/themes/paralivros/footer.php
+++ dynamic/wp-content/themes/paralivros/footer.php
@@ -11,14 +11,14 @@
 		) );
 		?>
 		<ul class="foot-social">
-			<li class="social-item"><a data-icon="f" href="#" target="_blank" title="Facebook">Facebook</a></li>
-			<li class="social-item"><a data-icon="t" href="#" target="_blank" title="Twitter">Twitter</a></li>
-			<li class="social-item"><a data-icon="i" href="#" target="_blank" title="Instagram">Instagram</a></li>
-			<li class="social-item"><a data-icon="p" href="#" target="_blank" title="Pinterest">Pinterest</a></li>
-			<li class="social-item"><a data-icon="g" href="#" target="_blank" title="Google+">Google+</a></li>
-			<li class="social-item"><a data-icon="s" href="#" target="_blank" title="RSS">RSS</a></li>
-			<li class="social-item"><a data-icon="k" href="#" target="_blank" title="Skoob">Skoob</a></li>
-			<li class="social-item"><a data-icon="r" href="#" target="_blank" title="GoodReads">GoodReads</a></li>
+			<li class="social-item"><a data-icon="f" href="<?php echo FACEBOOK; ?>" target="_blank" title="Facebook">Facebook</a></li>
+			<li class="social-item"><a data-icon="t" href="<?php echo TWITTER; ?>" target="_blank" title="Twitter">Twitter</a></li>
+			<li class="social-item"><a data-icon="i" href="<?php echo INSTAGRAM; ?>" target="_blank" title="Instagram">Instagram</a></li>
+			<li class="social-item"><a data-icon="p" href="<?php echo PINTEREST; ?>" target="_blank" title="Pinterest">Pinterest</a></li>
+			<li class="social-item"><a data-icon="g" href="<?php echo GOOGLEPLUS; ?>" target="_blank" title="Google+">Google+</a></li>
+			<li class="social-item"><a data-icon="s" href="<?php bloginfo( 'rss2_url' ) ?>" target="_blank" title="RSS">RSS</a></li>
+			<li class="social-item"><a data-icon="k" href="<?php echo SKOOB; ?>" target="_blank" title="Skoob">Skoob</a></li>
+			<li class="social-item"><a data-icon="r" href="<?php echo GOODREADS; ?>" target="_blank" title="GoodReads">GoodReads</a></li>
 		</ul>
 		<strong class="site-name">
 			<img alt="<?php name() ?>" data-at2x="<?php url() ?>/img/logo-small@2x.png" height="26" src="<?php url() ?>/img/logo-small.png" width="214">
