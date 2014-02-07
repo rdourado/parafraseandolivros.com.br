a0957370bd54eea9be612cec6436b03982ad91b1
Rafael Dourado <rdourado@gmail.com>
2014-02-07 13:31:11 -0200

    Comment form placeholder.

--- dynamic/wp-content/themes/paralivros/comments.php
+++ dynamic/wp-content/themes/paralivros/comments.php
@@ -14,7 +14,24 @@
 				endif;
 				comment_form( array(
 					'title_reply' => 'Quer parafrasear também?',
-					'label_submit' => 'Enviar'
+					'label_submit' => 'Enviar',
+					'fields' => array(
+						'author' =>
+							'<p class="comment-form-author"><label for="author">Nome</label> ' .
+							( $req ? '<span class="required">*</span>' : '' ) .
+							'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+							'" size="30"' . $aria_req . ' placeholder="seu nome*" /></p>',
+						'email' =>
+							'<p class="comment-form-email"><label for="email">Email</label> ' .
+							( $req ? '<span class="required">*</span>' : '' ) .
+							'<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+							'" size="30"' . $aria_req . ' placeholder="seu email*" /></p>',
+						'url' =>
+							'<p class="comment-form-url"><label for="url">Site</label>' .
+							'<input id="url" name="url" type="url" value="' . esc_attr( $commenter['comment_author_url'] ) .
+							'" size="30" placeholder="site ou blog" /></p>',
+					),
+					'comment_field' => '<p class="comment-form-comment"><label for="comment">Comentário</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true" placeholder="escreva seu comentário"></textarea></p>'
 				) ); 
 				?>
 			</div>
